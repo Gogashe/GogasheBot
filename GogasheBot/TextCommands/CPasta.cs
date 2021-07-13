303c4982d73d9cdffff5a04d418c8bd463af9cd6
Gogashe <gogashekmsk.02@gmail.com>
2021-07-13 20:38:40 +0200

    Упростил поднятие бота из репы на других машинах

--- GogasheBot/TextCommands/CPasta.cs
+++ GogasheBot/TextCommands/CPasta.cs
@@ -11,8 +11,7 @@ namespace GogasheBot.TextCommands
         {
             try
             {
-                string[] Pastas = new string[31];
-                Pastas = Directory.GetFiles(Directory.GetCurrentDirectory() + @"\Pastes");
+                string[] Pastas = Directory.GetFiles(Directory.GetCurrentDirectory() + @"\Pastes");
                 Random rand = new Random((int)DateTime.Now.Ticks);
                 string Pasta = Pastas[rand.Next(0, Pastas.Length)];
                 StreamReader ReadPasta = new StreamReader(Pasta);
